30b19a9c92d4647555062d14902b6692dee06335
sungchul lee <lsungchul@gmail.com>
2016-04-21 11:36:39 -0700

    Feedforward network (Perceptron Network)

---
+++ backpropagation/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace backpropagation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxAnswer = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(311, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(89, 32);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(185, 24);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "60000";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Number of Pictures: (Max 60,000)";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(282, 53);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(149, 20);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(271, 244);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(152, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(55, 244);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 13;
+            this.textBox5.Text = "100";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(46, 225);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Number of Pictures: (Max 10,000)";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(336, 190);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(64, 29);
+            this.pictureBox3.TabIndex = 15;
+            this.pictureBox3.TabStop = false;
+            
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(255, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Answer:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(268, 190);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "Input:";
+            // 
+            // textBoxAnswer
+            // 
+            this.textBoxAnswer.Location = new System.Drawing.Point(323, 155);
+            this.textBoxAnswer.Name = "textBoxAnswer";
+            this.textBoxAnswer.Size = new System.Drawing.Size(100, 20);
+            this.textBoxAnswer.TabIndex = 20;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 194);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(61, 13);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Timer (ms): ";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(111, 192);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(76, 20);
+            this.textBox6.TabIndex = 22;
+            this.textBox6.Text = "0";
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(11, 54);
+            this.button8.Margin = new System.Windows.Forms.Padding(2);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(63, 41);
+            this.button8.TabIndex = 25;
+            this.button8.Text = "Training";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(149, 56);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(76, 20);
+            this.textBox7.TabIndex = 26;
+            this.textBox7.Text = "0.1";
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(80, 59);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 13);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "gradient rate:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(81, 82);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Epoch: ";
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(149, 82);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(76, 20);
+            this.textBox8.TabIndex = 29;
+            this.textBox8.Text = "1";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 104);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(113, 13);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "Hidden Layer Nodes : ";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(149, 109);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(76, 20);
+            this.textBox9.TabIndex = 31;
+            this.textBox9.Text = "120";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(18, 155);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 322);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxAnswer);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Perceptron N";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxAnswer;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ backpropagation/Form1.cs
@@ -0,0 +1,487 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Xml;
+
+namespace backpropagation
+{
+    // private Image imgTemp;
+    
+    public partial class Form1 : Form
+    {
+        int[,] wMatrix = new int[28 * 28, 28 * 28];
+        string path = Application.StartupPath + @"\..\train-images\";
+        double[][] imageInputData;
+        NNetworks network;
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+
+
+        public void button8_Click(object sender, EventArgs e)
+        {
+            
+
+            String imageFilePath = path + "train-images.idx3-ubyte";
+            String labelFilePath = path + "train-labels.idx1-ubyte";
+
+            FileStream fs = new FileStream(@imageFilePath, FileMode.OpenOrCreate);
+            FileStream fs1 = new FileStream(@labelFilePath, FileMode.OpenOrCreate);
+
+            BinaryReader r = new BinaryReader(fs);
+            BinaryReader r1 = new BinaryReader(fs1);
+
+            for (Int32 i = 0; i < 4; i++)
+            {
+
+                BitConverter.ToInt32(r.ReadBytes(4).Reverse().ToArray(), 0);
+            }
+
+            for (Int32 i = 0; i < 2; i++)
+            {
+
+                BitConverter.ToInt32(r1.ReadBytes(4).Reverse().ToArray(), 0);
+            }
+            int numImage = 60000;
+            if (textBox2.Text != null)
+            {
+                numImage = Int32.Parse(textBox2.Text);
+            }
+
+            double[][] target = new double[numImage][];
+            imageInputData = new double[numImage][];
+
+
+            for (int count = 0; count < numImage; count++)
+            {
+                byte[] data = new byte[28 * 28];
+                for (int i = 0; i < 28 * 28; i++)
+                {
+                    data[i] = r.ReadByte();
+                }
+
+                Bitmap bitmap = new Bitmap(28, 28);
+                int[] tempData = new int[28 * 28];
+                imageInputData[count] = new double[28*28];
+
+                
+                for (int row = 0; row < 28; row++)
+                {
+                    for (int col = 0; col < 28; col++)
+                    {
+                        bitmap.SetPixel(col, row, Color.FromArgb(255 - data[row * 28 + col], 255 - data[row * 28 + col], 255 - data[row * 28 + col]));
+
+                        if (data[row * 28 + col] == 0)
+                        {
+                            imageInputData[count][row * 28 + col] = 0;
+                        }
+                        else
+                        {
+                            imageInputData[count][row * 28 + col] = 1;
+                        }
+
+                    }
+                }
+                target[count] = new double[10];
+                int getlabelNum = Convert.ToInt32(r1.ReadByte());
+                for(int i=0; i<10;i++){
+                    if (getlabelNum == i)
+                    {
+                        target[count][i]=1;
+                    }
+                    else
+                    {
+                        target[count][i] = 0;
+                    }
+
+
+                }
+                
+
+                
+
+
+                textBox3.Text = "Making Matrix: " + (count + 1).ToString() + " / " + numImage.ToString();
+                textBox3.Refresh();
+                pictureBox1.Image = bitmap;
+                pictureBox1.Refresh();
+            }
+            r.Close();
+            fs.Close();
+            r1.Close();
+            fs1.Close();
+
+
+            int hNode = Convert.ToInt32(textBox9.Text);
+            network = new NNetworks();
+
+            double sensitive = Convert.ToDouble(textBox7.Text);
+            int epoch = Convert.ToInt32(textBox8.Text);
+
+            network.Neuron(hNode, sensitive);
+
+            for (int ep = 0; ep < epoch;ep++ )
+            {
+                textBox3.Text = "epoch:" + (ep+1).ToString(); ;
+                textBox3.Refresh();
+                    
+                for (int i = 0; i < numImage; i++)
+                {
+                    network.sigmoid(imageInputData[i]);
+                    network.learn(imageInputData[i], target[i]);
+
+                }
+                   
+                               
+
+            }
+            textBox3.Text = "Training Done" ;
+            textBox3.Refresh();
+            
+
+/////////////////////////
+
+         
+
+
+
+
+
+
+            
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            String testImgFile  = path + "t10k-images.idx3-ubyte";
+            String testlabelPath = "t10k-labels.idx1-ubyte";
+
+           // String testImgFile = path + "train-images.idx3-ubyte";
+           // String testlabelPath = path + "train-labels.idx1-ubyte";
+
+            FileStream fs = new FileStream(testImgFile, FileMode.OpenOrCreate);
+
+            BinaryReader r = new BinaryReader(fs);
+
+
+            FileStream fs1 = new FileStream(testlabelPath, FileMode.Open);
+            BinaryReader r1 = new BinaryReader(fs1);
+
+
+            //long length = r1.BaseStream.Length;
+            for (Int32 i = 0; i < 2; i++)
+            {
+
+                BitConverter.ToInt32(r1.ReadBytes(4).Reverse().ToArray(), 0);
+            }
+
+
+            for (Int32 i = 0; i < 4; i++)
+            {
+
+                BitConverter.ToInt32(r.ReadBytes(4).Reverse().ToArray(), 0);
+            }
+
+            int numImage = Convert.ToInt32(textBox5.Text);
+            int wCount=0;
+
+            for (int count = 0; count < numImage; count++)
+            {
+                Bitmap bitmap = new Bitmap(28, 28);
+                byte[] data = new byte[28 * 28];
+
+
+
+                double[] tempData = new double[28 * 28];
+                for (int i = 0; i < 28 * 28; i++)
+                {
+                    data[i] = r.ReadByte();//array[0];
+                }
+
+                for (int col = 0; col < 28; col++)
+                {
+
+                    for (int row = 0; row < 28; row++)
+                    {
+                        bitmap.SetPixel(col, row, Color.FromArgb(255 - data[(row) * 28 + col], 255 - data[(row) * 28 + col], 255 - data[(row) * 28 + col]));
+                        if (data[(row) * 28 + col] == 0)
+                        {
+                            tempData[(row) * 28 + col] = 0;
+                        }
+                        else
+                        {
+                            tempData[(row) * 28 + col] = 1;
+                        }
+                    }
+
+
+                }
+
+                pictureBox3.Image = bitmap;
+                pictureBox3.Refresh();
+
+
+
+
+                int testNumber = Convert.ToInt32(r1.ReadByte());
+
+
+               
+
+               
+
+                int hNodes = Convert.ToInt32(textBox9.Text);
+               
+                 double[] test = network.sigmoid(tempData);
+
+                   
+
+                
+                int getAnswer = 0;
+                double max = 0;
+                for (int i = 1; i < 10+1; i++)
+                {
+
+                    if (max < test[i])
+                    {
+                        max = test[i];
+                        getAnswer = i;
+                    }
+
+                }
+                double rating = test[getAnswer];
+
+
+                textBoxAnswer.Text = (getAnswer-1).ToString() + " (rate: " + rating.ToString("F3") + ")";
+                textBoxAnswer.Refresh();
+
+
+                if((getAnswer-1)!=testNumber)
+                {
+                    wCount++;
+                }
+
+                textBox4.Text = "Error rate: " + (((double)wCount / (double)(count+1))*100).ToString("F2") + " %";
+                textBox4.Refresh();
+
+
+                int timer = 0;
+
+                if (!textBox6.Text.Equals(""))
+                {
+                    timer = Convert.ToInt32(textBox6.Text);
+                }
+                System.Threading.Thread.Sleep(timer);
+
+
+
+            }
+            r.Close();
+            r1.Close();
+            fs1.Close();
+            fs.Close();
+
+
+
+
+
+
+        }
+
+
+
+
+    }
+
+
+    
+    class NNetworks
+    {
+        public NNetworks()
+        {
+
+        }
+        int hNode;
+        double[][] weight1;//=new double[10][];
+        double[][] weight;//=new double[10][];
+        double[] hidden;
+        double[] input;
+        double[] output;
+        double senstive;
+
+        public void Neuron(int hNode, double senstive)
+        {
+            this.senstive = senstive;
+            weight = new double[hNode+1][];
+            weight1 = new double[10 + 1][];
+            Random rnd = new Random();
+            this.hNode = hNode;
+
+            hidden = new double[hNode+1];
+            input = new double[28 * 28 + 1];
+            output = new double[10 + 1];
+
+
+
+
+
+
+            for (int j = 0; j < hNode + 1; j++)
+            {
+                weight[j] = new double[28*28+1];
+                for (int i = 0; i < 28*28+1; i++)
+                {
+                    weight[j][i] = rnd.NextDouble() * 2 - 1;
+                   
+                }
+            }
+            for (int j = 0; j < 10 + 1; j++)
+            {
+                weight1[j] = new double[hNode + 1];
+                for (int i = 0; i < hNode + 1; i++)
+                {
+                    weight1[j][i] = rnd.NextDouble() * 2 - 1;
+
+                }
+            }
+            
+        }
+        public double[] sigmoid(double[] inputData)
+        {
+          /*  double[] sum =new double[10];
+            for (int j = 0; j < 10; j++)
+            {
+
+                for (int i = 0; i < 28*28; i++)
+                {
+                    sum[j] = sum[j] + weight[j][i] * inputData[i];
+                }
+                sum[j] = sum[j] + weight[j][28*28] * 1.0;
+
+            }
+            
+            double[] returnValue = new double[10];
+            
+            for(int i=0;i<10;i++){
+                returnValue[i]= 1.0 / (1.0 + Math.Exp(-sum[i]));
+            
+            }
+            return returnValue;*/
+
+            input[0] = 1.0;
+            hidden[0] = 1.0;
+
+            for (int i = 0; i < 28*28; i++)
+            {
+                input[i + 1] = inputData[i];
+            }
+
+            for (int j = 1; j < hNode+1; j++)
+            {
+                hidden[j] = 0.0;
+                for (int i = 0; i < 28*28+1; i++)
+                {
+                    hidden[j] += weight[j][i] * input[i];
+                }
+                hidden[j] = 1.0 / (1.0 + Math.Exp(-hidden[j]));
+            }
+
+            // Passing through output layer
+            for (int j = 1; j < 10 +1; j++)
+            {
+                output[j] = 0.0;
+                for (int i = 0; i <= hNode; i++)
+                {
+                    output[j] += weight1[j][i] * hidden[i];
+                }
+                output[j] = 1.0 / (1 + Math.Exp(-output[j]));
+            }
+
+            return output;
+            
+
+
+        }
+
+        public void learn(double[] inputData, double[] target)
+        {
+            /*double[] output = new double[10];
+            double[] outputError = new double[10];
+            
+            for (int i = 0; i < 10; i++)
+            {
+                output=sigmoid(inputData);
+                outputError[i]= output[i] - target[i];
+
+            }
+            
+            for (int j = 0; j < 10; j++) { 
+                for (int i = 0; i < 28*28; i++)
+                {
+                    error[j][i] = error[j][i] + outputError[j] * inputData[i] * output[j] * (1 - output[j]);
+                }
+                error[j][28 * 28] = error[j][28 * 28] + outputError[j] * 1.0 * output[j] * (1 - output[j]);
+            }*/
+            double[] error2 = new double[10 + 1];
+            double[] error1 = new double[hNode + 1];
+            double sum = 0.0;
+
+            for (int i = 1; i < 10+1; i++)  
+                error2[i] = output[i] * (1.0 - output[i]) * ( output[i]-target[i - 1]);
+
+
+            for (int i = 0; i <= hNode; i++)
+            {  
+                for (int j = 1; j < 10+1; j++)
+                    sum += weight1[j][i] * error2[j];
+
+                error1[i] = hidden[i] * (1.0 - hidden[i]) * sum;
+                sum = 0.0;
+            }
+
+            //the gradient descent
+            for (int j = 1; j < 10+1; j++)
+                for (int i = 0; i < hNode+1; i++)
+                    weight1[j][i] -= senstive * error2[j] * hidden[i];
+
+            for (int j = 1; j < hNode+1; j++)
+                for (int i = 0; i < 28*28 +1; i++)
+                    weight[j][i] -= senstive * error1[j] * input[i];
+
+        }
+
+
+        
+      /*  public void fix()
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 0; i < weight.Length; i++)
+                {
+                    weight[j][i] = weight[j][i] - senstive * error[j][i];
+                    error[j][i] = 0.0;
+                }
+            }
+        }*/
+
+    }
+
+
+}
